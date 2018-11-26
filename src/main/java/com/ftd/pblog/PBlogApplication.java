181123bcf2646f9e49fbc5ed3b2578ceef3acf7a
Starrk <ning15845920609@163.com>
2018-11-26 11:06:29 +0800

    First commit

---
+++ src/main/java/com/ftd/pblog/PBlogApplication.java
@@ -0,0 +1,12 @@
+package com.ftd.pblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PBlogApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PBlogApplication.class, args);
+  }
+}
